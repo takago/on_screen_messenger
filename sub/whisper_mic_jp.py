83ce5f7085f338549b09b1f016038a62385b10ce
TAKAGO Lab <takago@neptune.kanazawa-it.ac.jp>
2023-11-24 16:54:22 +0900

    Update whisper_mic_jp.py

--- sub/whisper_mic_jp.py
+++ sub/whisper_mic_jp.py
@@ -1,4 +1,4 @@
 from whisper_mic.whisper_mic import WhisperMic
 
-mic = WhisperMic(model='small',device='cuda', pause=1.0)
+mic = WhisperMic(model='small',device='cuda', pause=0.7)
 mic.listen_loop()
